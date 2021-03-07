fc6bc7c41fb3522b97be22b33ee8d65923db3841
Vladyslav <buhlored@gmail.com>
2021-03-07 21:44:04 +0200

    add method stringInput in stringOperations

--- src/ua/hilel/hw5/stringOperations.java
+++ src/ua/hilel/hw5/stringOperations.java
@@ -1,4 +1,12 @@
 package ua.hilel.hw5;
 
+import java.util.Scanner;
+
 public class stringOperations {
+
+        public void stringInput() {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Input string: ");
+                String string1 = in.nextLine();
+        }
 }
