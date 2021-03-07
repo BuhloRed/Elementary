61f61ecb187fa36dd7c39c1c638519f2c994e3ca
Vladyslav <buhlored@gmail.com>
2021-03-07 23:06:10 +0200

    add cheking of even in evenCounter

--- src/ua.hilel.hw5/stringOperations.java
+++ src/ua.hilel.hw5/stringOperations.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class stringOperations {
 
         private String string1;
+        private boolean evenCheck;
 
         public void stringInput() {
                 Scanner in = new Scanner(System.in);
@@ -16,7 +17,11 @@ public class stringOperations {
                 for (int i = 0; i < string1.length(); i++) {
                         count++;
                 }
-                System.out.println(count);
+                if (count % 2==0)
+                        evenCheck = true;
+                else
+                        evenCheck = false;
+                System.out.println(evenCheck);
         }
         public void middleSymbol() {
 
