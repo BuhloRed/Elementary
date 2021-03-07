57ca3cc492b396f8d47f6a403e4f22aa2b2b3e4a
Vladyslav <buhlored@gmail.com>
2021-03-07 22:48:46 +0200

    add local variable string1 and method middleSymbol

--- src/ua.hilel.hw5/stringOperations.java
+++ src/ua.hilel.hw5/stringOperations.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class stringOperations {
 
+        private String string1;
+
         public void stringInput() {
                 Scanner in = new Scanner(System.in);
                 System.out.print("Input string: ");
-                String string1 = in.nextLine();
+                string1 = in.nextLine();
                 System.out.println(string1);
+        }
+        public void middleSymbol(){
+
         }
 }
